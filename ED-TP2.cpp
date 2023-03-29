66a334349702a5b57e8f5c614fa81f049a0103cf
tcallecram <118746940+tcallecram@users.noreply.github.com>
2023-03-29 14:12:31 -0300

    Add files via upload

---
+++ ED-TP2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <algorithm>
+using namespace std;
+
+/**Declarando variáveis*/
+
+string msg, msg1;
+int x, y;
+
+
+
+
+void gotoxy(short x, short y) 
+{
+	COORD coord = {x, y};
+	SetConsoleCursorPosition(GetStdHandle ( STD_OUTPUT_HANDLE ), coord);
+}
+
+void cascata(){
+	for(x=0;x <= msg.size(); x++){
+		
+		gotoxy(60, 0);
+		cout << msg1;
+		
+		msg1.replace(x, 1, " ");/*linha: posição que a string será realocada; 2: números de caracteres que serão reclocados da string principal; " ": será a string que será colocada no lugar da velha*/	
+		gotoxy(60,0);
+		cout <<msg1;/*mostra o valor msg1, que agora está realocado como " "*/
+		
+		
+		for(y=1; y <=20; y++){
+			gotoxy(60 + x, y);
+			cout<< msg[x];/*mostra o caracter de posição x do array string*/
+			
+			Sleep(50);/*permite a "animação" das letras caindo*/
+			
+			gotoxy(60 + x, y); /*aponta a localização certa para o if funcionar*/				
+			if (y < 20) /*caso y menor q linha 20, coloque " "*/
+			{
+				cout << " ";
+			}
+		}
+	}
+	
+}
+
+int main()
+{
+	gotoxy(45,0);
+	cout << "Entre uma mensagem: " << endl;
+	gotoxy(45,1);
+	cin >> msg;
+	
+	msg1 = msg;
+	
+	system("cls");
+	
+	cascata();
+	
+	return 0;
+	
+}
